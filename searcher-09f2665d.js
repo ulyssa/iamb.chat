7922f5315addd0c4e6ee25c8073c0b54ee99c602
ulyssa <ulyssa@users.noreply.github.com>
2026-09-21 14:53:09 +0000

    deploy: 302ea12703ba6aa72e3ac96b1fc3963717c3fcb2

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a9401921.js',
+                path_to_root + 'searchindex-49f74f72.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
